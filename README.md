c37a6915737d8c3db59e9383475e3fce46bdd3dd
Carsten Martens <carsten.martens@outlook.de>
2019-10-09 14:17:07 +0200

    Initial Release

---
+++ README.md
@@ -0,0 +1,9 @@
+Deutsche Beschreibung/German description
+-----------------------------------------------
+Essensplan für VIS
+
+ToDo:
+
+
+Changelog:
+08.10.2019 V0.0.1 Initial Release
---
+++ essensplan.js
@@ -0,0 +1,37 @@
+/**
+  ##########         ESSENSPLAN          ##########
+wöchentlicher Essensplan und Rezeptverwaltung für VIS
+
+  09.10.2019:   V0.0.1  Initialrelease (quick&Durty)
+
+  to do:
+
+  Author: Carsten Martens (carsten.martens@outlook.de)
+  License: GNU General Public License V3, 29. Juni 2007
+**/
+
+/**
+  ##########         Variablen          ##########
+**/
+
+// Informationen mitloggen?
+var DEBUG = true;
+
+/**
+  ##########         Pfade          ##########
+**/
+const JSPath                  = "javascript.0.";
+const Path                    = JSPath + 'Essensplan.';
+
+/**
+  ##########         Datenpunkte          ##########
+**/
+
+/**
+  ##########         Pfade          ##########
+**/
+
+
+/**
+  ##########         Funktionen          ##########
+**/
